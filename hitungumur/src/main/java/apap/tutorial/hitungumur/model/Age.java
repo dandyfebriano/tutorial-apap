c6db7b0f103e27851394c6a8512486595f7b404a
dandy <febrianod@gmail.com>
2019-09-17 15:38:17 +0700

    demo

--- hitungumur/src/main/java/apap/tutorial/hitungumur/model/Age.java
+++ hitungumur/src/main/java/apap/tutorial/hitungumur/model/Age.java
@@ -18,7 +18,7 @@ public class Age implements Serializable{
     }
 
     public double mercurianYears(){
-       return Math.floor(this.earthianYears()/ .24); 
+       return Math.floor(this.earthianYears()/ .32); 
     }
 
     public double venusianYears(){
